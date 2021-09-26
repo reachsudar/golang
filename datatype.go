ce58b30b9be3aacbe393ee322136c39b2e926af5
reachsudar <reachsudar@gmail.com>
2021-09-26 22:36:39 +1000

    datatypes

---
+++ datatype.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	//integers
+
+	var a int = 123
+	var b int = -123
+	var x uint = 500
+
+	//float type
+
+	var f1 float32 = 3.4e+38
+	var f2 float64 = 1.7e+308
+
+	//strings
+
+	var s1 string = "hello"
+
+	// bool
+
+	var b1 bool = true
+
+	fmt.Println(a, b, x)
+	fmt.Println(f1, f2)
+	fmt.Println(s1)
+	fmt.Println(b1)
+
+}
