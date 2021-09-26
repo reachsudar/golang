679c5aede9b2b92023089b6f501090ae10babbda
reachsudar <reachsudar@gmail.com>
2021-09-26 22:32:52 +1000

    variable

--- variables.go
+++ variables.go
@@ -2,18 +2,15 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 
-var a int = 2
-var b = 3
-var x ="hello"
+	var (
+		a int    = 1
+		b string = "hello"
+		c bool   = true
+	)
+	var x, y, z = 1, 34.5, "world"
 
- y:=4
-var p,q = 3,"Hello"
-
-fmt.Println(a,b,x)
-
-fmt.Println(y,p,q)
-
+	fmt.Println(a, b, c)
+	fmt.Println(x, y, z)
 }
-
